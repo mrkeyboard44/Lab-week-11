16096a1c5aed12173d5bfa3aa83e8773af75fde7
mrkeyboard44 <matthewdandar@gmail.com>
2021-03-26 22:54:28 -0700

    added last for loop for output

--- JS/app.js
+++ JS/app.js
@@ -3,10 +3,12 @@ const object1 = { todo: "clone repo for starter code", status:"complete" }
 const object2 = { todo: "push good commit messages", status:"started"}
 const object3 = { todo: "Create a for loop for todo list", status:"started"}
 let todoList = [object1, object2, object3]
-const completedTodos = []
+let completedTodos = []
 todoList.forEach(function(object) {
     if (object.status == 'complete') {
-        completedTodos.push(object.todo)
+        completedTodos.push(object)
     }
 })
-console.log('completed: ', completedTodos)
+completedTodos.forEach(function(object) {
+    console.log(object.todo)
+})
